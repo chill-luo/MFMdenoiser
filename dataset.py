8a7769caad95051bd422ef43b89b5eab50f41666
luoqinxuan <luoqinxuan2018@ia.ac.cn>
2024-11-19 19:37:25 +0800

    simplify

--- dataset.py
+++ dataset.py
@@ -4,14 +4,9 @@ import yaml
 import random
 import numpy as np
 import tifffile as tiff
-from tqdm import tqdm
 
 import torch
-from torch import nn
 from torch.utils.data import Dataset, DataLoader
-
-from model import UNet_n2n_un 
-from utils import calculate_sliding_std, get_shuffling_mask
 
 
 def normalize(x, mode='submean'):
@@ -97,107 +92,16 @@ def format(x, dtype):
     return x
 
 
-class SingleStackDataset(Dataset):
+class ContinuousStackDataset(Dataset):
     def __init__(self, config, mode='train'):
         self.norm = config['general']['norm']
         self.crop_size = config['train']['crop_size']
-        self.mode = mode
-
-        print('===> Data loaded.')
-        self.stack = tiff.imread(config['general']['raw_path'])
-        print(f'Stack: {self.stack.shape}')
-        self.dtype = self.stack.dtype
-        self.stack, param = normalize(self.stack, self.norm)
-        self.params = [param] * self.stack.shape[0]
-        self.shuffle_mask_stack = None
-
-        if config['general']['gt_path'] is not None:
-            self.gt_stack = tiff.imread(config['general']['gt_path'])
-            self.gt_stack = self.gt_stack.astype(np.float32)
-            if self.gt_stack.ndim == self.stack.ndim - 1:
-                self.gt_stack = np.expand_dims(self.gt_stack, axis=0).repeat(self.stack.shape[0], axis=0)
-            elif self.gt_stack.ndim != self.stack.ndim:
-                raise ValueError('The dimension of gt and raw should be the same.')
-        else:
-            self.gt_stack = np.zeros(self.stack.shape[0], dtype=np.float32)
-
-    def __len__(self):
-        return self.stack.shape[0]
-
-    def __getitem__(self, idx):
-        if self.mode == 'train':
-            if self.shuffle_mask_stack is None:
-                img = self.stack[idx]
-                if self.crop_size is not None:
-                    img = random_crop([img], self.crop_size)[0]
-                elif (img.shape[-2]%32 != 0) or (img.shape[-1]%32 != 0):
-                    img = center_crop(img)
-                img = torch.from_numpy(img).float().unsqueeze(0)
-                return idx, img
-            else:
-                img = self.stack[idx]
-                mask = self.shuffle_mask_stack[idx]
-                if self.crop_size is not None:
-                    img, mask = random_crop([img, mask], self.crop_size)
-                elif (img.shape[-2]%32 != 0) or (img.shape[-1]%32 != 0):
-                    img = center_crop(img)
-                    mask = center_crop(mask)
-                img = torch.from_numpy(img).float().unsqueeze(0)
-                mask = torch.from_numpy(mask).float().unsqueeze(0)
-                return idx, img, mask
-        elif self.mode == 'test':
-            img = self.stack[idx]
-            gt = self.gt_stack[idx]
-            if (img.shape[-2]%32 != 0) or (img.shape[-1]%32 != 0):
-                img = center_crop(img)
-                gt = center_crop(gt)
-            img = torch.from_numpy(img).float().unsqueeze(0)
-            gt = torch.from_numpy(gt).float().unsqueeze(0)
-            return idx, img, gt
-        else:
-            raise ValueError(f'No such mode: {self.mode}')
-
-    def generate_shuffle_masks(self, config):
-        upsampler = nn.Upsample(scale_factor=config['train']['std_kernel_size'], mode='nearest')
-        self.shuffle_mask_stack = np.zeros_like(self.stack)
-        os.makedirs(os.path.join(config['general']['save_dir'], config['general']['exp_name'], 'masks'), exist_ok=True)
-
-        model = UNet_n2n_un(1, 1).cuda()
-        model.load_state_dict(torch.load(os.path.join(config['general']['pth_dir'], config['general']['exp_name'], f'epoch_bound.pth')))
-        model.eval()
-
-        print('\n===> Generating shuffling masks...')
-        for i in tqdm(range(self.stack.shape[0])):
-            noisy_original_torch = torch.from_numpy(self.stack[i]).float().unsqueeze(0).unsqueeze(0).cuda()
-
-            avg =0.
-            for _ in range(config['eval']['num_predictions']):
-                with torch.no_grad():
-                    mask = torch.rand_like(noisy_original_torch)  # uniformly distributed between 0 and 1
-                    mask = (mask < (1. - config['general']['mask_ratio'])).float().cuda()
-                    output = model(mask * noisy_original_torch)
-                    to_img = output.detach().cpu().squeeze().squeeze().numpy()
-                avg += to_img
-            
-            denoised = inv_normalize(avg / float(config['eval']['num_predictions']), self.params[i], config['general']['norm'])
-            std_map_torch = calculate_sliding_std(torch.from_numpy(denoised).unsqueeze(0).unsqueeze(0).cuda(), config['train']['std_kernel_size'], 1)
-            shuffling_mask = get_shuffling_mask(std_map_torch,  config['train']['masking_threshold'])
-            self.shuffle_mask_stack[i] = shuffling_mask
-
-            tiff.imwrite(os.path.join(config['general']['save_dir'], config['general']['exp_name'], 'masks', f'{str(i+1).zfill(3)}_image.tif'), denoised)
-            tiff.imwrite(os.path.join(config['general']['save_dir'], config['general']['exp_name'], 'masks', f'{str(i+1).zfill(3)}_mask.tif'), shuffling_mask)
-
-
-class MultipleStackDataset(Dataset):
-    def __init__(self, config, mode='train'):
-        self.norm = config['general']['norm']
-        self.crop_size = config['train']['crop_size']
+        self.fus_num = config['train']['fus_num'] if mode == 'train' else 3
         self.mode = mode
         
         print('===> Data loaded.')
         self.params = []
         self.raw_list, self.dtype = self.scan_stack_files(config['general']['raw_dir'], norm=config['general']['norm'], info=True)
-        self.shuffle_mask_list = None
 
         if config['general']['gt_dir'] is not None:
             self.gt_list, _ = self.scan_stack_files(config['general']['gt_dir'], norm=None)
@@ -209,113 +113,6 @@ class MultipleStackDataset(Dataset):
 
     def __getitem__(self, idx):
         if self.mode == 'train':
-            if self.shuffle_mask_list is None:
-                img = self.raw_list[idx]
-                if self.crop_size is not None:
-                    img = random_crop([img], self.crop_size)[0]
-                elif (img.shape[-2]%32 != 0) or (img.shape[-1]%32 != 0):
-                    img = center_crop(img)
-                img = torch.from_numpy(img).float().unsqueeze(0)
-                return idx, img           
-            else:
-                img = self.raw_list[idx]
-                mask = self.shuffle_mask_list[idx]
-                if self.crop_size is not None:
-                    img, mask = random_crop([img, mask], self.crop_size)
-                elif (img.shape[-2]%32 != 0) or (img.shape[-1]%32 != 0):
-                    img = center_crop(img)
-                    mask = center_crop(mask)
-                img = torch.from_numpy(img).float().unsqueeze(0)
-                mask = torch.from_numpy(mask).float().unsqueeze(0)
-                return idx, img, mask           
-        elif self.mode == 'test':
-            img = self.raw_list[idx]
-            gt = self.gt_list[idx]
-            if (img.shape[-2]%32 != 0) or (img.shape[-1]%32 != 0):
-                img = center_crop(img)
-                gt = center_crop(gt)
-            img = torch.from_numpy(img).float().unsqueeze(0)
-            gt = torch.from_numpy(gt).float().unsqueeze(0)
-            return idx, img, gt       
-        else:
-            raise ValueError(f'No such mode: {self.mode}')
-    
-    def scan_stack_files(self, dir, norm=None, info=False):
-        files = os.listdir(dir)
-        files.sort()
-        stack_list = []
-        for i, f in enumerate(files):
-            if '.tif' not in f:
-                continue
-            stack = tiff.imread(os.path.join(dir, f))
-            dtype = stack.dtype
-            if info:
-                print(f'Stack {i+1}: {stack.shape}')
-
-            if norm is not None:
-                stack, param = normalize(stack, norm)
-                self.params += [param] * stack.shape[0]
-
-            for j in range(stack.shape[0]):
-                stack_list.append(stack[j].astype(np.float32))
-            
-        return stack_list, dtype
-
-    def generate_shuffle_masks(self, config):
-        upsampler = nn.Upsample(scale_factor=config['train']['std_kernel_size'], mode='nearest')
-        self.shuffle_mask_list = []
-        os.makedirs(os.path.join(config['general']['save_dir'], config['general']['exp_name'], 'masks'), exist_ok=True)
-
-        model = UNet_n2n_un(1, 1).cuda()
-        model.load_state_dict(torch.load(os.path.join(config['general']['pth_dir'], config['general']['exp_name'], f'epoch_bound.pth')))
-        model.eval()
-
-        print('\n===> Generating shuffling masks...')
-        for i in tqdm(range(len(self.raw_list))):
-            noisy_original_torch = torch.from_numpy(self.raw_list[i]).float().unsqueeze(0).unsqueeze(0).cuda()
-
-            avg =0.
-            for _ in range(config['eval']['num_predictions']):
-                with torch.no_grad():
-                    mask = torch.rand_like(noisy_original_torch)  # uniformly distributed between 0 and 1
-                    mask = (mask < (1. - config['general']['mask_ratio'])).float().cuda()
-                    output = model(mask * noisy_original_torch)
-                    to_img = output.detach().cpu().squeeze().squeeze().numpy()
-                avg += to_img
-            
-            denoised = inv_normalize(avg / float(config['eval']['num_predictions']), self.params[i], config['general']['norm'])
-            std_map_torch = calculate_sliding_std(torch.from_numpy(denoised).unsqueeze(0).unsqueeze(0).cuda(), config['train']['std_kernel_size'], 1)
-            shuffling_mask = get_shuffling_mask(std_map_torch,  config['train']['masking_threshold'])
-            self.shuffle_mask_list.append(shuffling_mask)
-
-            tiff.imwrite(os.path.join(config['general']['save_dir'], config['general']['exp_name'], 'masks', f'{str(i+1).zfill(3)}_image.tif'), format(denoised, self.dtype))
-            tiff.imwrite(os.path.join(config['general']['save_dir'], config['general']['exp_name'], 'masks', f'{str(i+1).zfill(3)}_mask.tif'), shuffling_mask)
-
-
-class ContinuousStackDataset(Dataset):
-    def __init__(self, config, mode='train'):
-        self.norm = config['general']['norm']
-        self.crop_size = config['train']['crop_size']
-        self.fus_num = config['train']['fus_num']
-        self.mode = mode
-        
-        print('===> Data loaded.')
-        self.params = []
-        self.raw_list, self.dtype = self.scan_stack_files(config['general']['raw_dir'], norm=config['general']['norm'], info=True)
-
-        if config['general']['gt_dir'] is not None:
-            self.gt_list, _ = self.scan_stack_files(config['general']['gt_dir'], norm=None)
-        else:
-            self.gt_list = np.zeros(len(self.raw_list), dtype=np.float32)
-
-    def __len__(self):
-        if self.mode == 'train':
-            return len(self.raw_list)
-        else:
-            return len(self.raw_list) + self.fus_num - 1
-
-    def __getitem__(self, idx):
-        if self.mode == 'train':
             stk = self.raw_list[idx]
             if self.crop_size is not None:
                 stk = random_crop(stk, self.crop_size)[0]
@@ -324,18 +121,14 @@ class ContinuousStackDataset(Dataset):
             stk = torch.from_numpy(stk).float()
             return idx, stk
         elif self.mode == 'test':
-            if idx < len(self.raw_list):
-                img = self.raw_list[idx][0]
-                gt = self.gt_list[idx][0]
-            else:
-                img = self.raw_list[len(self.raw_list)-1][idx-len(self.raw_list)+1]
-                gt = self.gt_list[len(self.raw_list)-1][idx-len(self.raw_list)+1]            
-            if (img.shape[-2]%32 != 0) or (img.shape[-1]%32 != 0):
-                img = center_crop(img)
+            stk = self.raw_list[idx]
+            gt = self.gt_list[idx][1]         
+            if (stk.shape[-2]%32 != 0) or (stk.shape[-1]%32 != 0):
+                stk = center_crop(stk)
                 gt = center_crop(gt)
-            img = torch.from_numpy(img).float().unsqueeze(0)
+            stk = torch.from_numpy(stk).float()
             gt = torch.from_numpy(gt).float().unsqueeze(0)
-            return idx//self.fus_num, img, gt
+            return idx, stk, gt
         else:
             raise ValueError(f'No such mode: {self.mode}')
     
@@ -372,7 +165,3 @@ if __name__ == '__main__':
     for batch in dataloader:
         idx, image = batch
         ipdb.set_trace()
-        # print(idx)
-        # print(len(dataset.params))
-        # print(dataset.params)
-        # print(image.shape)
--- utils.py
+++ utils.py
@@ -1,114 +1,7 @@
-import ipdb
 import random
 import openpyxl
-import numpy as np
-import tifffile as tiff
-from einops import rearrange
-from scipy.optimize import leastsq
-from skimage.metrics import structural_similarity as ssim
-from skimage.metrics import peak_signal_noise_ratio as psnr
 
 import torch
-
-
-def get_conv(kernel_size=3, stride=1):
-    padding_size = (kernel_size - 1) // 2
-    conv = torch.nn.Conv2d(in_channels=1, 
-                           out_channels=1, 
-                           kernel_size=kernel_size, 
-                           stride=stride, 
-                           padding=padding_size, 
-                           bias=False,
-                           padding_mode='reflect')
-    conv.weight = torch.nn.Parameter((torch.ones((1, 1, kernel_size, kernel_size))/(1.0 * kernel_size ** 2)).cuda())
-    return conv
-
-
-def smooth(noisy, kernel_size=3, stride=1):
-    conv = get_conv(kernel_size, stride)
-    b, c, h, w = noisy.shape
-    smoothed = conv(noisy.view(-1, 1, h, w))
-    _, _, new_h, new_w = smoothed.shape     
-    return smoothed.view(1, c, new_h, new_w).detach()
-
-
-def reproduce(seed):
-    random.seed(seed)
-    np.random.seed(seed)
-    torch.manual_seed(seed)
-
-
-def calculate_sliding_std(img, kernel_size=3, stride=1):   
-    slided_mean = smooth(img, kernel_size, stride)
-    # mean_upsampled = upsampler(slided_mean)
-    variance = smooth((img - slided_mean)**2, kernel_size, stride)
-    # upsampled_variance = upsampler(variance)
-    return variance.sqrt()
-
-
-def shuffle_input(img, indices, mask, c, h, w, k):
-    if c == 1:
-        img_torch = torch.from_numpy(img).unsqueeze(0)
-    else:
-        img_torch = torch.from_numpy(img)
-    mask_torch = torch.from_numpy(mask).unsqueeze(0).repeat(c, 1, 1)
-    img_torch_rearranged = rearrange(img_torch.unsqueeze(1), 'c 1 (h1 h) (w1 w) -> c (h1 w1) h w ', h1=h//k, w1=w//k) # (c H//k W//k k k)
-    mask_torch_rearranged = rearrange(mask_torch.unsqueeze(1), 'c 1 (h1 h) (w1 w) -> c (h1 w1) h w ', h1=h//k, w1=w//k)
-    img_torch_rearranged = img_torch_rearranged.view(c, -1, k*k)# (c H//k*W//k k*k)
-    mask_torch_rearranged, _ = torch.max(mask_torch_rearranged.view(c, -1, k*k), 2, keepdim=True)
-    img_torch_reordered = torch.gather(img_torch_rearranged.clone(), dim=-1, index=indices).clone()
-    img_torch_reordered_v2 = img_torch_reordered.view(c, -1, k*k)
-    # Shuffle the image only at the flat regions (where mask = 0)
-    img_torch_final = mask_torch_rearranged * img_torch_rearranged + (1 - mask_torch_rearranged) * img_torch_reordered_v2
-    img_torch_final = img_torch_final.view(c, -1, k, k)    
-    img_torch_final_v2 = rearrange(img_torch_final, 'c (h1 w1) h w -> c 1 (h1 h) (w1 w) ', h1=h//k, w1=w//k)
-    return img_torch_final_v2.squeeze().cpu().numpy() 
-
-
-def get_shuffling_mask(std_map_torch, threshold=0.5):
-    std_map = std_map_torch.cpu().numpy().squeeze()
-    normalized = std_map/std_map.max()
-    thresholded = np.zeros_like(normalized)
-    thresholded[normalized >= threshold] = 1.
-    return thresholded
-
-
-def generate_random_permutation(h, w, c, factor):
-    d1, d2, d3 = c, (h//factor)*(w//factor), factor*factor
-    permutaion_indices = torch.argsort(torch.rand(1, d2, d3), dim=-1)
-    permutaion_indices = permutaion_indices.repeat(d1, 1, 1)
-    reverse_permutation_indices = torch.argsort(permutaion_indices, dim=-1)
-
-    return permutaion_indices, reverse_permutation_indices
-
-
-def get_mask_ratio(estimated_std_list, config):
-    diff = np.mean(np.abs(np.array(estimated_std_list[0])- np.array(estimated_std_list[1])))
-
-    if diff > config['epsilon_high']:
-        apply_local_shuffling=True
-        mask_ratio = config['mask_high']
-    elif diff < config['epsilon_low']:
-        apply_local_shuffling = False
-        mask_ratio = config['mask_low']
-    else:
-        apply_local_shuffling = False
-        mask_ratio = config['mask_medium']
-    return mask_ratio, apply_local_shuffling
-
-
-def shuffle_image(images, masks, config):
-    n, h, w = images.shape
-    noisy_shuffled_torch = torch.zeros(n, 1, h, w)
-
-    i = 0
-    for image, mask in zip(images, masks):
-        permutation_indices, _ = generate_random_permutation(h, w, 1, config['train']['shuffling_tile_size'])
-        noisy_shuffled_np= shuffle_input(image,  permutation_indices, mask=mask, c=1, h=h, w=w, k=config['train']['shuffling_tile_size'])
-        noisy_shuffled_torch[i] = torch.from_numpy(noisy_shuffled_np).unsqueeze(0)
-        i += 1
-
-    return noisy_shuffled_torch
 
 
 def slice_fusion(stack, rand=False):
@@ -130,59 +23,6 @@ def slice_fusion(stack, rand=False):
         fused_stack[k] = torch.sum(stack[k] * fuse_mask, dim=0, keepdim=True)
 
     return fused_stack
-
-
-def simpleNormalize(x):
-    tmp = x.astype(np.float32).flatten()
-    tmp.sort()
-    p1 = tmp[int(len(tmp) * 0.001)]
-    p2 = tmp[int(len(tmp) * 0.999)]
-    x = (x - p1) / (p2 - p1 + 1e-8)       
-    return x
-
-
-def consistent_translate(x, y, norm):
-
-    def func(p, x):
-        k, b = p
-        return k * x + b
-    
-    def error(p, x, y):
-        return func(p, x) - y
-    
-    if norm:
-        x = simpleNormalize(x)
-        y = simpleNormalize(y)
-    else:
-        x = x.astype(np.float32)
-        y = y.astype(np.float32)
-    
-    factor = y.mean() / (x.mean() + 1e-8)
-    k_init = np.random.normal(factor, 1)
-    b_init = np.random.normal(0, 1)
-    k, b = leastsq(error, [k_init, b_init], args=(x.flatten(), y.flatten()))[0]
-    
-    x_t = k * x + b
-    return x_t, y, (k, b)
-
-
-def compare(image, target, alignment=False):
-    def vrange(x):
-        if x.max() <= 255:
-            return 255
-        else:
-            return x.max() - x.min()
-
-    if alignment:
-        image_ssim, target_ssim, _ = consistent_translate(image, target, norm=True)
-        image_psnr, target_psnr, _ = consistent_translate(image, target, norm=False)
-        score_ssim = ssim(image_ssim, target_ssim, data_range=1)
-        score_psnr = psnr(target_psnr, image_psnr, data_range=vrange(target_psnr))
-    else:
-        score_ssim = ssim(image.astype(np.float32), target.astype(np.float32), data_range=vrange(target))
-        score_psnr = psnr(target.astype(np.float32), image.astype(np.float32), data_range=vrange(target))
-
-    return score_ssim, score_psnr
 
 
 class QueueList():
@@ -215,24 +55,17 @@ class XlsBook():
         self.book.save(save_path)
 
 
-if __name__ == '__main__':
-    # stack = torch.tensor([[[[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]],
-    #                       [[-1,-2,-3,-4],[-5,-6,-7,-8],[-9,-10,-11,-12],[-13,-14,-15,-16]],
-    #                       [[101,102,103,104],[105,106,107,108],[109,110,111,112],[113,114,115,116]]]])
-    # print(stack.shape)
-    # print(slice_fusion(stack))
-    # print(slice_fusion(stack).shape)
+# if __name__ == '__main__':
+#     stack = torch.tensor([[[[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]],
+#                           [[-1,-2,-3,-4],[-5,-6,-7,-8],[-9,-10,-11,-12],[-13,-14,-15,-16]],
+#                           [[101,102,103,104],[105,106,107,108],[109,110,111,112],[113,114,115,116]]]])
+#     print(stack.shape)
+#     print(slice_fusion(stack))
+#     print(slice_fusion(stack).shape)
 
-    # stack = tiff.imread('data/ConvA/raw/conva.tif')[None, ...]
-    # stack = torch.from_numpy(stack.astype(np.float32))
-    # substack = stack[:, :2, ...]
-    # fusedsub = slice_fusion(substack).numpy()[0].astype(np.uint16)
-    # outp = np.concatenate((fusedsub, substack.numpy()[0].astype(np.uint16), fusedsub, fusedsub), axis=0)
-    # tiff.imwrite('tmp.tif', outp)
-
-    outp = tiff.imread('results/conva_base_09/epoch_200/001.tif')
-    denoised = outp[1]
-    # denoised = tiff.imread('data/ConvA/raw/conva.tif')[0]
-    gt = tiff.imread('data/ConvA/gt_single.tif')
-    score_ssim, score_psnr = compare(denoised, gt)
-    print(score_ssim, score_psnr)
+#     stack = tiff.imread('data/ConvA/raw/conva.tif')[None, ...]
+#     stack = torch.from_numpy(stack.astype(np.float32))
+#     substack = stack[:, :2, ...]
+#     fusedsub = slice_fusion(substack).numpy()[0].astype(np.uint16)
+#     outp = np.concatenate((fusedsub, substack.numpy()[0].astype(np.uint16), fusedsub, fusedsub), axis=0)
+#     tiff.imwrite('tmp.tif', outp)
